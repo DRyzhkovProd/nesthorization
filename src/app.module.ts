89902c4d6dbcaa9b53dd5094382a2447a06b0b2f
ryzhkov.d.a <ryzhkov.d.a@sblogistica.ru>
2022-07-26 16:35:57 +0300

    :wrench: add auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,19 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
-import { getMongoConfig } from './config/mongo.config';
+import { getMongoConfig } from './configs/mongo.config';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
+      isGlobal: true,
       envFilePath: ['.env', '.env.development', '.env.development.local'],
     }),
     AuthModule,
     MongooseModule.forRootAsync(getMongoConfig()),
+    UserModule,
   ],
-
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
